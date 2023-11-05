571e89ff7f209768ba59850a1894de91df44f5a5
Nathalee Wilund <nathalee.wilund@edu.varberg.se>
2023-11-05 12:14:55 +0100

    Skapat klassen Geometry (Parent class)

---
+++ Geometry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUT23_Labb3_Poly
+{
+    internal class Geometry
+    {
+        public double _area;
+        public virtual double Area(double area)
+        {
+            _area = area;
+            return area;
+        }
+    }
+}
